05f53a917113648a81ab96f353800d64dd9e6ec9
Maximilian Keßler <git@maximilian-kessler.de>
2021-10-18 15:55:26 +0200

    fix extension mistake in package formatter class

--- default_formatters/package_formatter.py
+++ default_formatters/package_formatter.py
@@ -5,7 +5,7 @@ import PyTeX.macros
 
 class PackageFormatter(PyTeX.formatter.TexFormatter):
     def __init__(self, package_name: str, author: str = PyTeX.base.DEFAULT_AUTHOR, extra_header: [str] = []):
-        PyTeX.formatter.TexFormatter.__init__(self, package_name, author, extra_header, '.cls')
+        PyTeX.formatter.TexFormatter.__init__(self, package_name, author, extra_header, '.sty')
 
     def make_default_macros(self):
         PyTeX.macros.make_default_macros(self, 'package')
